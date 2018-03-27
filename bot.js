341b4ece3b7633c123bc301364f1e6a9c7d4236b
Codetopher <37638720+Codetopher@users.noreply.github.com>
2018-03-26 20:48:50 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+let messageObject = {
+    sent: "Your aid has been sent.",
+    deny: "Your aid has been denied. You may contact the Collectors for more information."
+   
+};
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on("message", message => {
+    if(message.content.toLowerCase().startsWith("!dm")) {
+        let args = message.content.split(" ").slice(1);
+        let user = message.guild.member(args[0].replace(/[<@!>]/g,"")).user;
+        let keyword= args[1];
+
+        if(keyword in messageObject){
+            return user.send(messageObject[keyword]);
+        }
+        return message.channel.send("That is not a valid keyword!");
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
